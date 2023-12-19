5adfba1fef158aedba536b9790adaa7da5d506a6
mac <a12@Shahils-MacBook-Pro.local>
2023-12-19 16:27:23 +0530

    updated

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,49 @@
 import React, { Component } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-// you can explore more - and check as how to use materiul ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// implement the class below
 class Carousel extends Component {
-  
+
+        constructor(){
+            super()
+            this.state={
+                index:0
+            }
+        }
+
+        next = () => {
+            this.setState((prevState) => ({
+                index: prevState.index !== images.length - 1 ? prevState.index + 1 : 0
+            }));
+        };
+        
+        previous = () => {
+            this.setState((prevState) => ({
+                index: prevState.index !== 0 ? prevState.index - 1 : images.length - 1
+            }));
+        };
+        
+    
+
+
+    render() {
+        return (
+            <div className="main">
+                <ArrowBackIosIcon className="arrow-icon" fontSize="large" className='leftarrow' onClick={this.next}/>
+                <div className="image-container">
+                    <div className="textbox">
+                    <h1 className="title">{images[this.state.index].title}</h1>
+                    <p className="subtitle">{images[this.state.index].subtitle}</p>
+                    </div>
+                    <img src={images[this.state.index].img} />
+                    
+                </div>
+                <ArrowForwardIosIcon className="arrow-icon" fontSize="large" className='rightarrow' onClick={this.previous}/>
+            </div>
+        );
+    }
 }
 
 export default Carousel;
